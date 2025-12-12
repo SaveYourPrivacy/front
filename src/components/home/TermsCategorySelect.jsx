83355613fcec16e3f991b6e87a764f92b199095c
ohige01 <ohige01@naver.com>
2025-12-12 19:24:40 +0900

    [FIX] 약관 종류 선택 value 변경

--- src/components/home/TermsCategorySelect.jsx
+++ src/components/home/TermsCategorySelect.jsx
@@ -10,29 +10,29 @@ function TermsCategorySelect({ onSelect }) {
         console.log(category);
         onSelect(value);
     };
-
+    
     return (
         <div className="terms-category-radio">
             <h3>약관 종류 선택</h3>
             <div className="radio-group">
                 <div className="radio-item">
-                    <input type="radio" name="category" value="환불 및 해지 조항" onChange={handleSelect} />
+                    <input type="radio" name="category" value="환불" onChange={handleSelect} />
                     <span>환불 및 해지 조항</span>
                 </div>
                 <div className="radio-item">
-                    <input type="radio" name="category" value="자동결제 / 정기구독 조항" onChange={handleSelect} />
+                    <input type="radio" name="category" value="자동결제" onChange={handleSelect} />
                     <span>자동결제 / 정기구독 조항</span>
                 </div>
                 <div className="radio-item">
-                    <input type="radio" name="category" value="개인정보 수집 및 저장" onChange={handleSelect} />
+                    <input type="radio" name="category" value="개인정보" onChange={handleSelect} />
                     <span>개인정보 수집 및 저장</span>
                 </div>
                 <div className="radio-item">
-                    <input type="radio" name="category" value="서비스 책임 제한 / 면책 조항" onChange={handleSelect} />
+                    <input type="radio" name="category" value="책임 제한" onChange={handleSelect} />
                     <span>서비스 책임 제한 / 면책 조항</span>
                 </div>
                 <div className="radio-item">
-                    <input type="radio" name="category" value="광고, 제3자 제공, 데이터 활용" onChange={handleSelect} />
+                    <input type="radio" name="category" value="광고" onChange={handleSelect} />
                     <span>광고, 제3자 제공, 데이터 활용</span>
                 </div>
                 <div className="radio-item">
